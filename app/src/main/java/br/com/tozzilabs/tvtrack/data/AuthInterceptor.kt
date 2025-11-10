fe6b3037c28db8ff747d4bb23fffb74017e5d82a
vitortozzi <vitor.tozzi@gmail.com>
2025-11-10 16:18:23 -0300

    Migration AGP, gradle kts files and gradle wrapper,

--- app/src/main/java/br/com/tozzilabs/tvtrack/data/AuthInterceptor.kt
+++ app/src/main/java/br/com/tozzilabs/tvtrack/data/AuthInterceptor.kt
@@ -9,10 +9,10 @@ class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val httpUrl = original.url.newBuilder()
-            .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
             .build()
 
         val requestBuilder: Request.Builder = original.newBuilder()
+            .header("Authorization", value = "Bearer ${BuildConfig.TMDB_API_KEY}")
             .url(httpUrl)
 
         return chain.proceed(requestBuilder.build())
---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "7.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+}
